e39b7410893a58a6085c0d422fa68f59a2005c81
dievusance <divyanshum2000@gmail.com>
2024-10-22 21:56:03 +0530

    Learn string

---
+++ variables.py
@@ -0,0 +1,35 @@
+x=9 #int
+y="hello" #str
+print(x)
+print(y)
+# Strings
+
+# slice
+# b = " Hello, World! "
+# #print(b[2:8])
+# #print(b[0:8])
+
+# capitalLetters = b.upper()
+# print(capitalLetters)
+
+# c = "DEOIFBGOI"
+# small = c.lower()
+# print(small) 
+
+# # remove white space
+# spaceRemove = b.strip()
+# print(spaceRemove)
+
+
+# concatenate
+a = "Hello my"
+b = "World l"
+c = a + " " + b
+print(c)
+
+
+age = 36
+# txt = "My name is John, I am " + 36
+txt = f"My name is John, I am {age}"
+fef = f"The price is {20 * 59} dollars"
+print(fef)
